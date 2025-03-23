03b9c69c9e22814cb189a193ea454cd6a3ba97de
Xinyu <zhuxinyu1997@gmail.com>
2025-03-23 15:59:35 +1300

    added the solution for problem 53

---
+++ c#/53-MaxSubArray/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _53_MaxSubArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+            Console.WriteLine(MaxSubArray(nums));
+        }
+
+        public static int MaxSubArray(int[] nums)
+        {
+            int max = 0;
+            int res = nums[0];
+            foreach (int i in nums)
+            {
+                if (max < 0)
+                {
+                    max = 0;
+                }
+                max += i;
+                res = Math.Max(res, max);
+            }
+
+            
+            return res;
+        }
+    }
+}

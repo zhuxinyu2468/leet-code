e76238ff1211d714262ed8aead55bb77f6d01f82
Xinyu <zhuxinyu1997@gmail.com>
2025-03-23 15:40:15 +1300

    added the implementation of problem 67

--- c#/67-AddBinary/Program.cs
+++ c#/67-AddBinary/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace _67_AddBinary
+﻿using System.Text;
+
+namespace _67_AddBinary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string a = "11";
+            string b = "1";
+            string result = AddBinary(a, b);
+            Console.WriteLine(result);
+        }
+        public static string AddBinary(string a, string b)
+        {
+            string ans = string.Empty;
+            int carry = 0;
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            while (i >= 0 || j >= 0||carry==1) {
+                if (i >= 0)
+                {
+                    carry += a[i--] - '0';
+                }
+                if (j >= 0)
+                {
+                    carry += b[j--] - '0';
+                }
+
+                ans += carry % 2;
+                carry /= 2;
+            }
+            
+            return new string(ans.Reverse().ToArray()); 
         }
     }
 }

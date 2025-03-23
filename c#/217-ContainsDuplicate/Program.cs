2bcf4799474c935f17e155021850a4d613049ac8
Xinyu <zhuxinyu1997@gmail.com>
2025-03-23 15:49:39 +1300

    added the new solution for problem 217

---
+++ c#/217-ContainsDuplicate/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _217_ContainsDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1, 2, 3, 4, 5, 1];
+            Console.WriteLine(ContainsDuplicate(nums));
+        }
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+/*            HashSet<int> set = new HashSet<int>();
+            foreach (int i in nums)
+            {
+                if (set.Contains(i))
+                {
+                    return true;
+                }
+                else
+                {
+                    set.Add(i);
+                } 
+            }
+            return false;*/
+
+            // or 
+            HashSet<int> set = new HashSet<int>(nums);
+            return set.Count < nums.Length;
+        }
+    }
+}

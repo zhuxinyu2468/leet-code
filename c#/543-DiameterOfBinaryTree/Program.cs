b2e84d1a3d5475ee19c9ee15861f682802504ec2
Xinyu <zhuxinyu1997@gmail.com>
2025-03-23 14:13:13 +1300

    added the question 67 and 543 into the solution

---
+++ c#/543-DiameterOfBinaryTree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _543_DiameterOfBinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

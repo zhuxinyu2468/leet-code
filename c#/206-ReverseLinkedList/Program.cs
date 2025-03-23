df9a0cd6ea9e141051d5fa73721319a1ce240581
Xinyu <zhuxinyu1997@gmail.com>
2025-03-23 13:09:26 +1300

    added the new 206 leetcode question solution

---
+++ c#/206-ReverseLinkedList/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _206_ReverseLinkedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode listNode = new ListNode(1);
+            listNode.next = new ListNode(2);
+            listNode.next.next = new ListNode(3);
+            listNode.next.next.next = new ListNode(4);
+            listNode.next.next.next.next = new ListNode(5);
+            Console.WriteLine(ReverseList(listNode));
+        }
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode current = head;
+            ListNode next = null;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            return prev;
+        }
+    }
+}

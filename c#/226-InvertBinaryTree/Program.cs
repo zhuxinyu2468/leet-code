10553ebe96bf513f0780b9a2ebda59c7957a3041
Xinyu <zhuxinyu1997@gmail.com>
2025-04-13 12:15:44 +1200

    implemented the question 226

---
+++ c#/226-InvertBinaryTree/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _226_InvertBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null,
+            TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode testCase = new TreeNode(2);
+            testCase.left = new TreeNode(1);
+            testCase.left.left = new TreeNode(7);
+            testCase.left.right = new TreeNode(8);
+            testCase.right = new TreeNode(3);
+            testCase.right.left = new TreeNode(9);
+            testCase.right.right = new TreeNode(4);
+            Console.WriteLine(InvertTree(testCase));
+        }
+
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            InvertTree(root.left);
+            InvertTree(root.right);
+            return root;
+        }
+    }
+}

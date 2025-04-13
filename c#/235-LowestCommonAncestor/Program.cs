f259ed1bb92a8ba258e41dbc5e5d3552b539d17a
Xinyu <zhuxinyu1997@gmail.com>
2025-04-14 00:09:22 +1200

    implemented the question 235

---
+++ c#/235-LowestCommonAncestor/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _235_LowestCommonAncestor
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static TreeNode LowestCommonAncestor(
+            TreeNode root, TreeNode p, TreeNode q)
+        {
+            while (root != null)
+            {
+                if (root.val > p.val && root.val > q.val)
+                {
+                    root = root.left;
+                }
+                else if (root.val < p.val && root.val < q.val)
+                {
+                    root = root.right;
+                }
+                else
+                {
+                    return root;
+                }
+            }
+            return root;
+        }
+    }
+}
